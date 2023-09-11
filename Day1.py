d1e58c127d8dc3c9a18d1bbc61f102059b55c66c
Parth Kalbhor <103450785+Parth54321@users.noreply.github.com>
2023-09-11 22:44:42 +0530

    Day1.py
    Print adittion of two numbers without using 3rd variable

---
+++ Day1.py
@@ -0,0 +1,13 @@
+#====================First_Method==========================
+a = int(input("Enter first number "))
+b = int(input("Enter second number "))
+print(a+b)
+
+#====================Second_Method=========================
+print((int(input("Enter number ")))+(int(input("Enter number "))))
+
+#====================Third_Method=========================
+a = 5
+b = 3
+a = a + b
+print("The sum is:", a)
